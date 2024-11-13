15f8766b5338abe40d69b391da690050186464e8
Adam Hamar <adam.hamar@gmail.com>
2024-11-13 20:10:43 +0100

    Insert the todo panel into the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+
+import { ThemeService } from './theme.service';
+import { TodoPanelComponent } from './todo-panel/todo-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe, TodoPanelComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-todo-app';
+
+  constructor(public themeService: ThemeService) {}
 }
